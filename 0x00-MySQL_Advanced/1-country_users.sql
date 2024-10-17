437ac8aa8d9f38a45cf3b4a5e1e4552f43854866
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-17 21:53:22 +0100

    Fixed sql syntax

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Creates a table users with id, email, name, country
 DROP TABLE IF NOT EXISTS users;
 CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-  email VARCHAR(255) NOT NULL UNIQUE
-  name VARCHAR(255)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
   country CHAR(2) NOT NULL DEFAULT 'US' CHECK (COUNTRY IN ('US', 'CO', 'TN'))
 );
