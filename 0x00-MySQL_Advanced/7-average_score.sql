1920073f188cb2657d752dcfaa82e4108b11218b
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-18 12:19:37 +0100

    Task 7 - take 3

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,12 +4,12 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER |
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-  DECLARE avg_score FLOAT DEFAULT 0;
-
-  SELECT AVG(score) INTO avg_score FROM corrections WHERE corrections.user_id = 2;
-
   UPDATE users
-  SET users.average_score = avg_score
+  SET users.average_score = IFNULL((
+    SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id
+  ), 0)
   WHERE users.id = user_id;
 END |
 DELIMITER ;
