69934b89b4163f4e2a2bf847b66a1acbf7f80d30
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-25 05:12:22 +0100

    Completed task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module '11-schools_by_topic.py' contains function schools_by_topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic."""
+    topic_filter = {
+        "topics": {
+            "$elemMatch": {
+                "$eq": topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
