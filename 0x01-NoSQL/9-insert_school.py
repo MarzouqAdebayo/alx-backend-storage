2e30a28d13aee245fccedcba4c1c4afa5b1447e3
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-25 05:11:55 +0100

    Completed task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module '9-insert_school.py' contains function insert_school """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection."""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
