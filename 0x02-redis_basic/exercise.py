1156a9cc675777208e5a5a0903c865ee571d8450
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-25 08:55:31 +0100

    Implemented get, get_str and get_int methods on Cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module 'exercise.py' contains class Cache """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> None:
+        """Gets data using key from redis"""
+        data = self._redis.get(key)
+        if not fn:
+            return data
+        return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """Gets and decodes to string using get method"""
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Gets and converts to int using get method """
+        return self.get(key, lambda x: int(x))
