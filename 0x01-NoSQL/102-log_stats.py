4e34f78f1a0345e7594e764151898e5e05f94674
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-25 06:39:15 +0100

    Completed Tasks 15, modified task 12

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -22,6 +22,16 @@ def print_nginx_request_logs(nginx_collection):
         list(nginx_collection.find({"method": "GET", "path": "/status"}))
     )
     print("{} status check".format(status_checks_count))
+    pipeline = [
+        {"$group": {"_id": "$ip", "total_requests": {"$sum": 1}}},
+        {"$sort": {"total_requests": -1}},
+        {"$limit": 10},
+    ]
+    request_logs = nginx_collection.aggregate(pipeline)
+    for request_log in request_logs:
+        ip_address = request_log.get("_id")
+        request_count_from_ip_address = request_log.get("total_requests")
+        print("\t{}: {}".format(ip_address, request_count_from_ip_address))
 
 
 def run():
