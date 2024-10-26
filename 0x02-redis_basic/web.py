8110f27f375dda65263b8e6fb319ae913af54db6
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-26 05:47:08 +0100

    Completed advanced task, create a data cache function to track requests to a url and its result

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module 'web.py' contains function get_page """
+from functools import wraps
+from typing import Callable
+import redis
+import requests
+
+
+store = redis.Redis()
+
+
+def track_url_access(method: Callable) -> Callable:
+    """Counts the number of times a method is called"""
+
+    @wraps(method)
+    def wrapper(url: str) -> str:
+        """Increments call counter, then returns method call"""
+        store.incr("count:{}".format(url))
+        result = store.get("result:{}".format(url))
+        if result:
+            return result.decode("utf-8")
+        result = method(url)
+        store.set("count:{}".format(url))
+        store.setex("result:{}".format(url), 10, result)
+        return result
+
+    return wrapper
+
+
+@track_url_access
+def get_page(url: str) -> str:
+    """Return the content of a url and
+    tracks the number of times it was access and also caches its result"""
+    return requests.get(url).text
